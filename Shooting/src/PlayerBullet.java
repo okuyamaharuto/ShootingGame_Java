ce4e65fde6365a837170c125d9b480bf18e2da3f
Okuyama Haruto <oku127haru0127@gmail.com>
2024-06-17 11:41:10 +0900

    練習問題10-1

--- Shooting/src/PlayerBullet.java
+++ Shooting/src/PlayerBullet.java
@@ -3,4 +3,9 @@ public class PlayerBullet extends Character{
 public PlayerBullet(double x, double y, double vx, double vy) {
 	super(x,y,vx,vy);
 }
+public void draw(MyFrame f) {
+	f.setColor(200, 50, 50);
+	f.fillRect(x+10, y, 10, 20);
+	
+}
 }
