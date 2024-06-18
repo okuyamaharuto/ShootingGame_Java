fb6be08e09ccf0dd93406ab56bea679fe22015f1
Okuyama Haruto <oku127haru0127@gmail.com>
2024-06-18 10:06:00 +0900

    11.2 敵を動かす

--- Shooting/src/Enemy.java
+++ Shooting/src/Enemy.java
@@ -1,4 +1,9 @@
 
-public class Enemy {
+public class Enemy extends Character{
+
+	public Enemy(double x, double y, double vx, double vy) {
+		super(x, y, vx, vy);
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
 
 }
--- Shooting/src/StraightEnemy.java
+++ Shooting/src/StraightEnemy.java
@@ -1,4 +1,9 @@
 
-public class StraightEnemy {
+public class StraightEnemy extends Enemy{
+
+	public StraightEnemy(double x, double y, double vx, double vy) {
+		super(x, y, vx, vy);
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
 
 }
