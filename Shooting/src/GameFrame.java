25dd487527ce1f39094085fa8d6cffeaac0cca90
Okuyama Haruto <oku127haru0127@gmail.com>
2024-06-24 12:15:11 +0900

    13.1 すべての敵を倒したら「クリア」を表示する

--- Shooting/src/GameFrame.java
+++ Shooting/src/GameFrame.java
@@ -15,6 +15,10 @@ public void run() {
 		moveEnemies();
 		checkPlayerAndEnemies();
 		checkPlayerBulletsAndEnemies();
+		if(GameWorld.enemies.size()==0) {
+			setColor(0,0,0);
+			drawString("クリア!",100,200,40);
+		}
 		sleep(0.03);
 	}
 	}
@@ -24,6 +28,15 @@ public void moveEnemies() {
 		Enemy e = GameWorld.enemies.get(i);
 		e.draw(this);
 		e.move();
+	}
+	int i = 0;
+	while(i<GameWorld.enemies.size()) {
+		Enemy e = GameWorld.enemies.get(i);
+		if(e.y>400) {
+			GameWorld.enemies.remove(i);
+		}else {
+			i++;
+		}
 	}
 }
 
