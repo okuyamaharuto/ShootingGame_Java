9b830bb0a49bb95e9ffaa23729d4eaf4bdbe179c
Okuyama Haruto <oku127haru0127@gmail.com>
2024-06-24 10:06:42 +0900

    12.1 プレイヤーと敵の衝突判定

--- Shooting/src/GameFrame.java
+++ Shooting/src/GameFrame.java
@@ -13,10 +13,12 @@ public void run() {
 		GameWorld.player.move();
 		movePlayerBullets();
 		moveEnemies();
+		checkPlayerAndEnemies();
+		
 		sleep(0.03);
 	}
+	}
 	
-}
 public void moveEnemies() {
 	for(int i = 0; i<GameWorld.enemies.size(); i++) {
 		Enemy e = GameWorld.enemies.get(i);
@@ -35,6 +37,18 @@ public void movePlayerBullets() {
 			GameWorld.playerBullets.remove(i);
 		}else {
 			i++;
+		}
+	}
+	
+
+}
+public void checkPlayerAndEnemies() {
+	for(int i= 0; i<GameWorld.enemies.size(); i++) {
+		Enemy e=GameWorld.enemies.get(i);
+		if(Math.abs(e.x-GameWorld.player.x)<=30 &&
+		   Math.abs(e.y-GameWorld.player.y)<=30) {
+			System.out.println("やられた!");
+			GameWorld.player.y =-1000;
 		}
 	}
 }
